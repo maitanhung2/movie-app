54a199db1a0a8da1007dfb66b4607b5ac7ce20de
locDEV169 <loc16910@gmail.com>
2021-10-12 23:09:35 +0700

    connectDB Mongoose + Rest APi

--- api/index.js
+++ api/index.js
@@ -1,6 +1,16 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+const mongoose = require('mongoose');
+const dotenv = require("dotenv")
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("DB conntection Sucessfully"))
+.catch(err => console.log(err));
 
 app.listen(port, () => {
     console.log(`BE server running is  ${port} `)
