f73b6ce76a888e55d2382c34c0502d2134159480
locDEV169 <loc16910@gmail.com>
2021-10-21 08:49:09 +0700

    update api #4

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -8,11 +8,11 @@ router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
-    password: CryptoJS.AES.encrypt(
-      req.body.password,
-      process.env.SECRET_KEY
-    ).toString(),
-    //password: req.body.password,
+    // password: CryptoJS.AES.encrypt(
+    //   req.body.password,
+    //   process.env.SECRET_KEY
+    // ).toString(),
+    password: req.body.password,
   });
   try {
     const user = await newUser.save();
---
+++ api/test/unit-test/index.test.js
@@ -0,0 +1,50 @@
+// const request = require("supertest");
+// const app = require("../app");
+
+// Mock axios, else you will really request the endpoint
+// jest.mock("axios");
+// import axios from "axios";
+
+// beforeAll((done) => {
+//   request(app)
+//     .post("/api/auth/login")
+//     .send({
+//       username: user,
+//       password: pw,
+//     })
+//     .end((err, response) => {
+//       token = response.body.token; // save the token!
+//       done();
+//     });
+// });
+
+describe(" Post /api/auth/login", () => {
+  // test("should respond with a 200 status code", async () => {
+  //   const response = await request(app).post("/login").send({
+  //     username: "username",
+  //     password: "password",
+  //   });
+  //   expect(response.statusCode).toBe(200);
+  // });
+  describe("login function", () => {
+    const email = "test@test.com";
+    const password = "password";
+
+    describe("with success", () => {
+      const data = { something: {} };
+
+      beforeEach(() => {
+        /*
+         * Not necessary for the moment, but will be useful
+         * to test successful & error response
+         */
+        //axios.post.mockResolvedValue({});
+      });
+
+      it("should call endpoint with given email & password", () => {});
+      test("It adds two numbers", () => {
+        expect(1 + 1).toBe(2);
+      });
+    });
+  });
+});
