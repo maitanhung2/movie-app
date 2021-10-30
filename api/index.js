8af319b219906b89d34aba1f403797749aa82886
locDEV169 <loc16910@gmail.com>
2021-10-30 20:59:44 +0700

    api CRUD movies

--- api/index.js
+++ api/index.js
@@ -1,26 +1,31 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
-const mongoose = require('mongoose');
-const dotenv = require("dotenv")
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
 
 dotenv.config();
-const db = 'mongodb+srv://root:1234@cluster0.eibio.mongodb.net/netflix?retryWrites=true&w=majority'
+const db =
+  "mongodb+srv://root:1234@cluster0.eibio.mongodb.net/netflix?retryWrites=true&w=majority";
 //mongoose.connect(process.env.MONGO_URL,{
-mongoose.connect(process.env.MONGO_URL,{
+mongoose
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     //useCreateIndex: true,
-}).then(() => console.log("DB success connect "))
-.catch(err => console.log(err));
+  })
+  .then(() => console.log("DB success connect "))
+  .catch((err) => console.log(err));
 
-app.use(express.json())
+app.use(express.json());
 
-app.use("/api/auth",authRoute)
-app.use("/api/users",userRoute)
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
 
 app.listen(port, () => {
-    console.log(`BE server running is  ${port} `)
-})
+  console.log(`BE server running is  ${port} `);
+});
