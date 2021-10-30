8178b01dd938a5c32ecfacefb82de8c50b22f3da
locDEV169 <loc16910@gmail.com>
2021-10-30 14:18:04 +0700

    CRUD api user

--- api/index.js
+++ api/index.js
@@ -4,19 +4,22 @@ const port = process.env.PORT || 8080;
 const mongoose = require('mongoose');
 const dotenv = require("dotenv")
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 dotenv.config();
 const db = 'mongodb+srv://root:1234@cluster0.eibio.mongodb.net/netflix?retryWrites=true&w=majority'
 //mongoose.connect(process.env.MONGO_URL,{
 mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log("DB conntection Sucessfully"))
+    useUnifiedTopology: true,
+    //useCreateIndex: true,
+}).then(() => console.log("DB success connect "))
 .catch(err => console.log(err));
 
 app.use(express.json())
 
 app.use("/api/auth",authRoute)
+app.use("/api/users",userRoute)
 
 app.listen(port, () => {
     console.log(`BE server running is  ${port} `)
